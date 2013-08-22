f5501275b9d7358cc877f699ffd3cf279c744372
Alexandr <d-ai@yandex.ru>
2013-08-23 01:37:32 +0400

    changing sql-table schema
    (cherry picked from commit cb9084d4b4caf22a29bed421b1bdbab45f0832a6) (only php code, not plugin)
    Conflicts:
    	game_upload/scripting/sourcecomms.sp

--- web_upload/application/plugins/SourceComms/CommsPlugin.php
+++ web_upload/application/plugins/SourceComms/CommsPlugin.php
@@ -5,67 +5,92 @@ class CommsPlugin extends SBPlugin
 	{
 		return 'SourceComms';
 	}
-	
+
 	public function getDescription()
 	{
 		return Yii::t('CommsPlugin.main', 'Extended, temporary and permanent punishments');
 	}
-	
+
 	public function getAuthor()
 	{
 		return 'ppalex';
 	}
-	
+
 	public function getVersion()
 	{
-		return '1.0';
+		return '1.0.1';
 	}
-	
+
 	public function getUrl()
 	{
 		return 'http://www.tf2news.ru';
 	}
-	
-	
+
+
 	public function runInstall()
 	{
-		Yii::app()->db->createCommand()->createTable('{{comms}}', array(
-			'id' => 'int(5) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY',
-			'type' => 'tinyint(3) unsigned NOT NULL DEFAULT 0',
-			'steam' => 'varchar(32) DEFAULT NULL',
-			'name' => 'varchar(64) DEFAULT NULL',
-			'reason' => 'varchar(255) NOT NULL',
-			'length' => 'mediumint(8) unsigned NOT NULL',
-			'server_id' => 'smallint(5) unsigned DEFAULT NULL',
-			'admin_id' => 'smallint(5) unsigned DEFAULT NULL',
-			'admin_ip' => 'varchar(15) NOT NULL',
-			'unban_admin_id' => 'smallint(5) unsigned DEFAULT NULL',
-			'unban_reason' => 'varchar(255) DEFAULT NULL',
-			'unban_time' => 'int(10) unsigned DEFAULT NULL',
-			'create_time' => 'int(10) unsigned NOT NULL',
-			'KEY server_id (server_id)',
-			'KEY admin_id (admin_id)',
-			'KEY unban_admin_id (unban_admin_id)',
-		), 'ENGINE=InnoDB DEFAULT CHARSET=utf8');
+		$transaction = Yii::app()->db->beginTransaction();
+
+		try {
+			Yii::app()->db->createCommand()->createTable('{{comms}}', array(
+				'id' => 'mediumint(8) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY',
+				'type' => 'tinyint(1) unsigned NOT NULL DEFAULT 0',
+				'steam' => 'varchar(32) DEFAULT NULL',
+				'name' => 'varchar(64) DEFAULT NULL',
+				'reason' => 'varchar(255) NOT NULL',
+				'length' => 'mediumint(8) unsigned NOT NULL',
+				'server_id' => 'smallint(5) unsigned DEFAULT NULL',
+				'admin_id' => 'smallint(5) unsigned DEFAULT NULL',
+				'admin_ip' => 'varchar(15) NOT NULL',
+				'unban_admin_id' => 'smallint(5) unsigned DEFAULT NULL',
+				'unban_reason' => 'varchar(255) DEFAULT NULL',
+				'unban_time' => 'int(10) unsigned DEFAULT NULL',
+				'create_time' => 'int(10) unsigned NOT NULL',
+				'KEY server_id (server_id)',
+				'KEY admin_id (admin_id)',
+				'KEY unban_admin_id (unban_admin_id)',
+			), 'ENGINE=InnoDB DEFAULT CHARSET=utf8');
+			$transaction->commit();
+			return true;
+		}
+
+		catch(Exception $e)
+        {
+            $transaction->rollback();
+            return false;
+        }
 	}
-	
+
 	public function runUninstall()
 	{
-		Yii::app()->db->createCommand()->dropTable('{{comms}}');
+		$transaction = Yii::app()->db->beginTransaction();
+
+        try
+        {
+            Yii::app()->db->createCommand()->dropTable('{{comms}}');
+
+            $transaction->commit();
+            return true;
+        }
+
+        catch(Exception $e)
+        {
+            $transaction->rollback();
+            return false;
+        }
 	}
-	
-	
+
 	public function onBeginRequest($event)
 	{
 		// Register controller
 		Yii::app()->controllerMap['comms'] = $this->getPathAlias('controllers.CommsController');
-		
+
 		// Add URL rule
 		Yii::app()->urlManager->addRules(array(
 			'comms' => 'comms/index',
 		), false);
 	}
-	
+
 	public function onBeforeAction($action)
 	{
 		// Add header tab
