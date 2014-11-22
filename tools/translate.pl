e1e68a60037ac353ba6a12201c2cfdea745f184f
Alexandr Duplishchev <d-ai@ya.ru>
2014-11-22 18:38:36 +0300

    removed hardcoded paths from update translations tool

--- tools/translate.pl
+++ tools/translate.pl
@@ -4,6 +4,7 @@ use warnings;
 use strict;
 
 use File::Slurp qw/edit_file_lines/;
+use FindBin qw/$Bin/;
 
 print "Bumping plugin version\n";
 edit_file_lines {
@@ -12,16 +13,16 @@ edit_file_lines {
         $build++;
         $_ =~ s/\.\d+';$/.$build';/
     }
-} '/Users/alex/Code/SourceComms/web_upload/application/plugins/SourceComms/CommsPlugin.php';
+} "$Bin/../web_upload/application/plugins/SourceComms/CommsPlugin.php";
 
-
+my $path_to_yiic = $ARGV[1] || '/Users/alex/Code/SourceBans/web/framework/yiic';
 print "Extcract translations\n";
-system ('php /Users/alex/Code/SourceBans/web/framework/yiic message /Users/alex/Code/SourceComms/web_upload/application/plugins/SourceComms/messages/config.php') == 0 or die "An error occurred\n";
+system ("php $path_to_yiic message $Bin/../web_upload/application/plugins/SourceComms/messages/config.php") == 0 or die "An error occurred\n";
 
 print "Delete not-sourcecomms translation files\n";
-system ('rm /Users/alex/sc/web_upload/application/plugins/SourceComms/messages/*/sourcebans.php') == 0 or die;
-system ('rm /Users/alex/sc/web_upload/application/plugins/SourceComms/messages/*/zii.php') == 0 or die;
-system ('rm /Users/alex/sc/web_upload/application/plugins/SourceComms/messages/*/yii.php') == 0 or die;
+system ("rm $Bin/../web_upload/application/plugins/SourceComms/messages/*/sourcebans.php") == 0 or die;
+system ("rm $Bin/../web_upload/application/plugins/SourceComms/messages/*/zii.php") == 0 or die;
+system ("rm $Bin/../web_upload/application/plugins/SourceComms/messages/*/yii.php") == 0 or die;
 
 print "All done\n";
 exit;
